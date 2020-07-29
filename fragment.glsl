d3bd8de565484631bdd0be3e57fe7053ad8db080
kotfind <kotfind@yandex.ru>
2020-07-29 20:21:48 +0300

    textures

--- fragment.glsl
+++ fragment.glsl
@@ -1,9 +1,13 @@
 #version 330 core
 
 out vec4 fragColor;
-in vec3 myColor;
 
-void main()
-{
-    fragColor = vec4(myColor, 1.0f);
+in vec3 myColor;
+in vec2 texCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main() {
+    fragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.5) * vec4(myColor, 1.);
 }
--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <SDL2/SDL_opengl.h>
 #include <iostream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 using namespace std;
 
 SDL_Window *win;
@@ -18,6 +21,8 @@ struct Shader {
         shader = glCreateShader(type);
         this->type = type;
     }
+
+    operator GLuint() const { return shader; }
 
     void load(const char* path) {
         FILE *file = NULL;
@@ -71,9 +76,28 @@ struct Shader {
 struct ShaderProgram {
     GLuint program;
 
-    ShaderProgram() { program = glCreateProgram(); }
+    ShaderProgram(const char *vertexShaderPath, const char *fragmentShaderPath) {
+        program = glCreateProgram();
 
-    void attachShader(const Shader *shader) { glAttachShader(program, shader->shader); }
+        Shader *vertexShader = new Shader(GL_VERTEX_SHADER);
+        vertexShader->load(vertexShaderPath);
+        vertexShader->compile();
+
+        Shader *fragmentShader = new Shader(GL_FRAGMENT_SHADER);
+        fragmentShader->load(fragmentShaderPath);
+        fragmentShader->compile();
+
+        attachShader(vertexShader);
+        attachShader(fragmentShader);
+        link();
+
+        delete vertexShader;
+        delete fragmentShader;
+    }
+
+    operator GLuint() const { return program; }
+
+    void attachShader(const Shader *shader) { glAttachShader(program, *shader); }
 
     void link() {
         glLinkProgram(program);
@@ -92,24 +116,44 @@ struct ShaderProgram {
         }
     }
 
-    void setupAll(const char *vertexShaderPath, const char *fragmentShaderPath) {
-        Shader *vertexShader = new Shader(GL_VERTEX_SHADER);
-        vertexShader->load(vertexShaderPath);
-        vertexShader->compile();
+    void use() { glUseProgram(program); }
 
-        Shader *fragmentShader = new Shader(GL_FRAGMENT_SHADER);
-        fragmentShader->load(fragmentShaderPath);
-        fragmentShader->compile();
+    void set1i(const char *name, const int val) { glUniform1i(glGetUniformLocation(program, name), val); }
+};
 
-        attachShader(vertexShader);
-        attachShader(fragmentShader);
-        link();
+struct Texture {
+    GLuint texture;
 
-        delete vertexShader;
-        delete fragmentShader;
+    Texture(const char *path, const GLenum format, const GLenum loadformat) {
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        int width, height, nrChannels;
+        stbi_set_flip_vertically_on_load(true);
+        unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0); 
+        if (!data) {
+            cout << "ERROR::TEXTURE::IMAGE_CANNOT_BE_LOADED" << endl;
+
+            SDL_GL_DeleteContext(cont);
+            SDL_DestroyWindow(win);
+            SDL_Quit();
+            exit(1);
+
+            stbi_image_free(data);
+        }
+
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, loadformat, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        stbi_image_free(data);
     }
 
-    void use() { glUseProgram(program); }
+    operator int() const { return texture; }
 };
 
 int main() {
@@ -123,19 +167,20 @@ int main() {
     cont = SDL_GL_CreateContext(win);
 
     // Setting up shaders
-    ShaderProgram *program = new ShaderProgram();
-    program->setupAll("vertex.glsl", "fragment.glsl");
+    ShaderProgram *program = new ShaderProgram("vertex.glsl", "fragment.glsl");
 
     // Setting up vertices, VBO & VAO
     float vert[] = {
-        // coordinates       // color
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,
+        // Coordinates        // Colors           // Texture coordinates
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
     };
 
     unsigned int indices[] = {
-        0, 1, 2,
+        0, 1, 3,
+        1, 2, 3,
     };
 
     GLuint VAOs[1], VBOs[1], EBOs[1];
@@ -144,7 +189,6 @@ int main() {
     glGenBuffers     (1, EBOs);
 
     glBindVertexArray(VAOs[0]);
-
         glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[0]);
 
@@ -152,14 +196,25 @@ int main() {
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
         // Coordinates attrib
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
 
         // Colors attrib
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
         glEnableVertexAttribArray(1);
 
+        // Texture coordinates attrib
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
     glBindVertexArray(0);
+
+    // Setting up textures
+    Texture *texture1 = new Texture("box.jpg", GL_RGB, GL_RGB);
+    Texture *texture2 = new Texture("face.jpg", GL_RGB, GL_RGB);
+
+    program->use();
+    program->set1i("texture1", 0);
+    program->set1i("texture2", 1);
 
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     for(;;) {
@@ -181,6 +236,11 @@ int main() {
         // Render
         glClearColor(0.2f, 0.3f, 0.3f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, *texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, *texture2);
 
         program->use();
         glBindVertexArray(VAOs[0]);
